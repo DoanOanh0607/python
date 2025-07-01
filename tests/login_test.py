dc09e2e00dc19f777ffb4f4bf68feb1661ffc248
Oanh Doan <doanoanh1994@gmail.com>
2025-07-01 19:53:53 +0700

    modify code

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,4 +1,4 @@
-import pytest
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from tests.base_test import BaseTest
@@ -7,6 +7,8 @@ from pages.login_page import LoginPage
 class TestLogin(BaseTest):
     def test_login(self):
         login_page = LoginPage(self.driver)
-        login_page.login("Admin", "admin123")
+        login_page.enter_username("Admin")
+        login_page.enter_password("admin123")
         login_page.click_login()
+        assert "dashboard" in self.driver.current_url.lower()
 
